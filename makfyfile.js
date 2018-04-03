2fa558a4e6a0f0c4d20f92e5d23234718f117752
Wout Mertens <Wout.Mertens@gmail.com>
2018-04-03 20:50:05 +0200

    also build json versions of defaults
    (fixes Jest runs)

--- makfyfile.js
+++ makfyfile.js
@@ -1,17 +1,30 @@
+const yaml = require('js-yaml')
 const fs = require('fs-extra')
+
+const doYaml = from => {
+	const doc = yaml.safeLoad(fs.readFileSync(from, 'utf8'))
+	return fs.writeFile(
+		from.replace('src', 'dist').replace('.yaml', '.json'),
+		JSON.stringify(doc, null, 2)
+	)
+}
 
 module.exports.commands = {
 	build: {
 		desc: 'Build Stratokit',
 		args: {watch: {type: 'flag'}},
-		run: async (exec, {watch}) => {
+		run: async (exec, {watch}, utils) => {
 			await fs.emptyDir('dist')
 			// transpile src/ to dist/
 			await exec(
-				`babel ${watch
-					? '--watch'
-					: ''} -s true --ignore '**/*.test.js' -D -d dist/ src/`
+				`babel ${
+					watch ? '--watch' : ''
+				} -s true --ignore '**/*.test.js' -D -d dist/ src/`
 			)
+			// yaml convert
+			const yamlFiles = await utils.expandGlobsAsync(['src/**/*.yaml'])
+			await fs.mkdirp('dist/config/defaults')
+			await Promise.all(yamlFiles.map(x => doYaml(x)))
 		},
 	},
 
